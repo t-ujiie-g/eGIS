86c6c1168a7b16042f084634498f094fa06bb533
t-ujiie-g <tom.ujiie23@gmail.com>
2024-02-04 20:48:44 +0900

    update: frontend overview create

---
+++ frontend/webgis-app/src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+    return (
+        <div>
+            <h1>
+                Home
+            </h1>
+        </div>
+    );
+}
---
+++ frontend/webgis-app/src/app/contents/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+    return (
+        <div>
+            <h1>
+                Contents
+            </h1>
+        </div>
+    );
+}
